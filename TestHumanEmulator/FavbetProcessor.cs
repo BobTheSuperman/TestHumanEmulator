11311a56b0122958952a6e2a5395d556e7cc21ec
DESKTOP-ATG2IPI\serge <sergey.babenko.boss@gmail.com>
2023-04-01 01:04:56 +0300

    Added errors parser and process bet method has been finished. Now script ready for 85 percent.

--- TestHumanEmulator/FavbetProcessor.cs
+++ TestHumanEmulator/FavbetProcessor.cs
@@ -49,16 +49,56 @@ namespace XHE
             return balance >= betAmount;
         }
 
-        protected override bool PlaceBet(string betUrl)
+        protected override bool PlaceBet(string betUrl, double betAmount)
         {
             if (!browser.navigate(betUrl))
                 return false;
 
-            browser.wait_for(15, 3);
+            span.wait_element_exist_by_attribute("class", "OutcomeButton_coef__290P9", 0);
+            browser.wait_for();
+            Thread.Sleep(2000);
 
-            span.click_by_attribute("class", "OutcomeButton_coef__290P9", 0);
+            Console.WriteLine("betClick: " + span.get_by_attribute("class", "OutcomeButton_coef__290P9", 0).meta_click());
+
+            input.get_by_attribute("class", "Input_input__FzRkn BetSumInput_input__3xT0O BetSumInput_withoutValue__3p5OP").meta_click();
+
+            MyKeyboard.Input($"{betAmount}");
+
+            if(IsErrorsExist())
+            {
+                return false;
+            }
+
+            button.click_by_attribute("class", "Button_button__WYSdr Button_primary__1OqiW Button_l__3KQSE Button_color1__3fuRd Button_disabled__2w937 Button_fullWidth__1vToY", 0);
 
             return true;
+        }
+
+        private bool IsErrorsExist()
+        {
+            bool result = false;
+
+            var errorDivs = div.get_by_attribute("class", "error", 0);
+
+            string errorSeparator = "<@@@b@r@@@>";
+
+            var errorNumbers = div.get_all_numbers_by_attribute("class", "error", 0);
+
+            if (errorNumbers.Length > 0)
+            {
+                result = true;
+
+                errorNumbers = errorNumbers[0].Split(errorSeparator);
+            }
+
+            foreach (var number in errorNumbers)
+            {
+                var error = div.get_inner_text_by_number(Int32.Parse(number));
+
+                echo(error);
+            }
+
+            return result;
         }
     }
 }
--- TestHumanEmulator/ProcessorBase.cs
+++ TestHumanEmulator/ProcessorBase.cs
@@ -14,28 +14,29 @@ namespace XHE
 
         //ToDo: replace the params to UserBetJobDto for our main app
         public void ProcessBet(string login, string password, double betAmount, string betUrl)
-        { 
-            //ClearBrowser();
+        {
+            SetBrowserSettings();
 
-            if(Login(login, password))
+            if (Login(login, password))
             {
                 if (CheckBalance(betAmount))
                 {
-                    PlaceBet(betUrl);
+                    PlaceBet(betUrl, betAmount);
                 }
             }
         }
 
         protected abstract bool Login(string login, string password);
-        protected abstract bool CheckBalance(double betAmaumt);
-        protected abstract bool PlaceBet(string betUrl);
+        protected abstract bool CheckBalance(double betAmaunt);
+        protected abstract bool PlaceBet(string betUrl, double betAmaunt);
 
-        private bool ClearBrowser()
+        private bool SetBrowserSettings()
         {
             browser.clear_address_bar_history();
             browser.clear_cache();
             browser.clear_history();
             browser.clear_cookies("");
+            browser.set_do_not_track();
 
             return true;
         }
